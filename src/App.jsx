a6c6cb91737a0189671cbe3a6da80cffe66e4faa
Meta Lekan <metadelekan@gmail.com>
2023-08-11 07:14:01 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Photo from './component/Photo';
 
 const App = () => {
   return (
-    <div className='App'>
-      <div className="container-fluid">
-        <h1 className='text-center'>Photo Search</h1>
+    <div className='App mb-4'>
+      <div className="container-fluid min-vh-100 mb-4">
+        <h1 className='text-center' onClick={console.log("Yes")}>Unsplash Photo Search</h1>
         <Photo />
       </div>
     </div>
--- src/component/Photo.jsx
+++ src/component/Photo.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {useCallback, useState} from 'react';
+import { useCallback, useState } from 'react';
 import { useEffect, useRef } from 'react';
 
 const API_URL = 'https://api.unsplash.com/search/photos';
@@ -13,36 +13,35 @@ const Photo = () => {
     const [totalPages, setTotalPages] = useState(0);
     const [errorMsg, setErrorMsg] = useState("")
 
-    const fetchImages = useCallback(async ()=> {
+    const fetchImages = useCallback(async () => {
         try {
             if (searchInput.current.value) {
                 setErrorMsg('')
-                const {data} = await axios.get(`${API_URL}?query=${searchInput.current.value}&page=${page}&per_page=${imagesPerPage}&client_id=${import.meta.env.VITE_API_KEY}`);
+                const { data } = await axios.get(`${API_URL}?query=${searchInput.current.value}&page=${page}&per_page=${imagesPerPage}&client_id=${import.meta.env.VITE_API_KEY}`);
 
                 setImages(data.results);
                 setTotalPages(data.total_pages);
-                clear();
-                console.log(data);
+                // clear();
+                // console.log(data);
             }
         } catch (error) {
             setErrorMsg("Error fetching images. Try again later.")
-            console.log(error);
         }
     }, [page]);
 
-    useEffect(()=> {
+    useEffect(() => {
         fetchImages();
     }, [fetchImages]);
 
-    const resetHandle = ()=> {
-        setPage[1];
+    const resetHandle = () => {
+        setPage(1);
         fetchImages();
     }
-    const searchPhoto = ()=> {
+    const searchPhoto = () => {
         resetHandle();
     };
-    
-    const searchWithEnter =(event) => {
+
+    const searchWithEnter = (event) => {
         if (event.key === "Enter") {
             fetchImages();
         }
@@ -52,67 +51,66 @@ const Photo = () => {
         resetHandle();
     };
 
-    const clear = ()=> {
-        searchInput.current.value = "";
-    }
+    // const clear = ()=> {
+    //     searchInput.current.value = "";
+    // }
 
-    // console.log("page",page );
+    console.log("page", page);
 
-  return (
-    <div className='Photo border border-3 shawdow-lg rounded-3 p-2 mb-4'>
-        {errorMsg && <p className='text-danger'>{errorMsg}</p>}
-        <div className="search">
-            <div className="search-box">
-                <div className="search-field">
-                    <input 
-                        placeholder={`Enter search keyword...`}
-                        className="input" 
-                        type="text" 
-                        ref={searchInput}
-                        onKeyUp={searchWithEnter}
-                    />
-                    <div className="search-box-icon">
-                        <button 
-                            onClick={searchPhoto}
-                            className="button btn-icon-content">
-                            <i className="search-icon">
-                                <svg xmlns="://www.w3.org/2000/svg" version="1.1" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" fill="#fff"></path></svg>
-                            </i>
-                        </button>
+    return (
+        <div className='Photo border border-2 shadow-lg rounded-2 bg-light p-2 my-4'>
+            {errorMsg && <p className='text-danger'>{errorMsg}</p>}
+            <div className="search shadow-sm">
+                <div className="search-box">
+                    <div className="search-field">
+                        <input
+                            placeholder={`Enter search keyword...`}
+                            className="input"
+                            type="text"
+                            ref={searchInput}
+                            onKeyUp={searchWithEnter}
+                        />
+                        <div className="search-box-icon">
+                            <button
+                                onClick={searchPhoto}
+                                className="button btn-icon-content">
+                                <i className="search-icon">
+                                    <svg xmlns="://www.w3.org/2000/svg" version="1.1" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" fill="#fff"></path></svg>
+                                </i>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div className="filters my-4 d-flex gap-2 justify-content-center">
-            <div className='btn btn-warning' onClick={()=> handleSelection("safari")}>Safari</div>
-            <div className='btn btn-warning' onClick={()=> handleSelection("coding")}>Coding</div>
-            <div className='btn btn-warning' onClick={()=> handleSelection("dark")}>Dark</div>
-            <div className='btn btn-warning' onClick={()=> handleSelection("model")}>Model</div>
-        </div>
+            <div className="filters my-4 d-flex gap-2 justify-content-center">
+                <div className='btn btn-secondary' onClick={() => handleSelection("safari")}>Safari</div>
+                <div className='btn btn-secondary' onClick={() => handleSelection("coding")}>Coding</div>
+                <div className='btn btn-secondary' onClick={() => handleSelection("dark")}>Dark</div>
+                <div className='btn btn-secondary' onClick={() => handleSelection("cars")}>Cars</div>
+            </div>
 
-        <div className="image_container">
-            
-            {
-                images.map(image => {
-                    return (
-                        <img 
-                        className='image'
-                        key={image.id}
-                        src={image.urls.small} 
-                        alt={image.alt_description}
-                        />
-                    )
-                })
-            }
-        </div>
+            <div className="image_container">
+                {
+                    images.map(image => {
+                        return (
+                            <img
+                                className='image'
+                                key={image.id}
+                                src={image.urls.small}
+                                alt={image.alt_description}
+                            />
+                        )
+                    })
+                }
+            </div>
 
-        <div className="page-buttons d-flex gap-2 mt-2">
-            {page > 1 && (<button className='btn btn-warning' onClick={()=> setPage(page - 1)}>Previous</button>)}
-            {page < totalPages && (<button className='btn btn-success' onClick={()=> setPage(page + 1)}>Next</button>)}
+            <div className="page-buttons d-flex gap-2 my-3">
+                {page > 1 && (<button className='btn btn-success' onClick={() => setPage(page - 1)}>Previous</button>)}
+                {page < totalPages && (<button className='btn btn-success' onClick={() => setPage(page + 1)}>Next</button>)}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Photo;
